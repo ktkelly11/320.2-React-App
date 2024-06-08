a9f2fa435ce18e5643580607d02a360485677e26
Katherine Kelly <ktkelly11@gmail.com>
2024-06-08 19:43:15 -0400

    Exports not working

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
